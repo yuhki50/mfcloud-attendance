84da922da4036813da15a4e74d99c528ca53bc90
yuhki50 <yuhki50@gmail.com>
2020-02-28 20:54:28 +0900

    Implement

--- README.md
+++ README.md
@@ -1,32 +1,41 @@
 mfcloud-attendance
 ==================
 
+コマンドラインからMFクラウドにログインして、出勤・退勤を行うCLIアプリケーションである。
 
 
-[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
-[![Version](https://img.shields.io/npm/v/mfcloud-attendance.svg)](https://npmjs.org/package/mfcloud-attendance)
-[![Downloads/week](https://img.shields.io/npm/dw/mfcloud-attendance.svg)](https://npmjs.org/package/mfcloud-attendance)
-[![License](https://img.shields.io/npm/l/mfcloud-attendance.svg)](https://github.com/yuhki50/mfcloud-attendance/blob/master/package.json)
+# Dependency
 
-<!-- toc -->
-* [Usage](#usage)
-* [Commands](#commands)
-<!-- tocstop -->
-# Usage
-<!-- usage -->
-```sh-session
-$ npm install -g mfcloud-attendance
-$ mfcloud-attendance COMMAND
-running command...
-$ mfcloud-attendance (-v|--version|version)
-mfcloud-attendance/0.0.0 darwin-x64 node-v13.8.0
-$ mfcloud-attendance --help [COMMAND]
-USAGE
-  $ mfcloud-attendance COMMAND
-...
+* node.js >= 8.0.0
+
+
+# Installation
+
+```bash
+$ git clone https://github.com/cami/mfcloud-attendance.git
+$ cd mfcloud-attendance
+$ npm install
 ```
-<!-- usagestop -->
-# Commands
-<!-- commands -->
 
-<!-- commandsstop -->
+
+# Usage
+
+## 出勤
+
+```bash
+./bin/run --company "your company" --email "your email" --password "your password" --arrive
+```
+
+
+## 退勤
+
+```bash
+./bin/run --company "your company" --email "your email" --password "your password" --leave
+```
+
+
+## ヘルプ
+
+```bash
+./bin/run -h
+```
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as puppeteer from 'puppeteer'
 import {Command, flags} from '@oclif/command'
 
 class MfcloudAttendance extends Command {
@@ -7,22 +8,68 @@ class MfcloudAttendance extends Command {
     // add --version flag to show CLI version
     version: flags.version({char: 'v'}),
     help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    // flag with a value (--company=VALUE)
+    company: flags.string({description: 'MFCloud company', required: true}),
+    // flag with a value (--email=VALUE)
+    email: flags.string({description: 'MFCloud email', required: true}),
+    // flag with a value (--password=VALUE)
+    password: flags.string({description: 'MFCloud password', required: true}),
+    // flag (--arrive)
+    arrive: flags.boolean({description: 'Arrive company'}),
+    // flag (--leave)
+    leave: flags.boolean({description: 'Leave company'}),
+    // flag with a value (-d, --delay)
+    delay: flags.integer({char: 'd', description: 'Delay [minute]'}),
+    // flag (-r, --random)
+    random: flags.boolean({char: 'r', description: 'Enable random delay time', dependsOn: ['delay']}),
   }
 
-  static args = [{name: 'file'}]
-
   async run() {
-    const {args, flags} = this.parse(MfcloudAttendance)
+    const {flags} = this.parse(MfcloudAttendance)
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    const browser = await puppeteer.launch({
+      args: [
+        '--lang=ja,en-US,en',
+        '--no-sandbox',
+        '--disable-setuid-sandbox'
+      ],
+    })
+
+    const page = await browser.newPage()
+
+    await page.emulate({
+      viewport: {
+        width: 1920,
+        height: 1080,
+      },
+      userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+    })
+
+    try {
+      await page.goto('https://attendance.moneyforward.com/employee_session/new')
+      await page.type('input#employee_session_form_office_account_name', flags.company || '')
+      await page.type('input#employee_session_form_account_name_or_email', flags.email || '')
+      await page.type('input#employee_session_form_password', flags.password || '')
+      await page.click('input[name=commit]')
+      await page.waitForSelector('h1.attendance-category-title', {timeout: 3000})
+
+      const minDelay = 0
+      const delay = flags.delay || minDelay
+      const randomDelay = flags.random ? Math.floor(Math.random() * delay) : delay
+      await page.waitFor((randomDelay > minDelay ? randomDelay : minDelay) * 1000 * 60)
+
+      if (flags.arrive) {
+        await page.click('ul.attendance-card-time-stamp-list li:nth-child(1) a.attendance-card-time-stamp-button.attendance-text-link') // 出勤
+      } else if (flags.leave) {
+        await page.click('ul.attendance-card-time-stamp-list li:nth-child(2) a.attendance-card-time-stamp-button.attendance-text-link') // 退勤
+      }
     }
+    catch (e) {
+      console.log(e)
+      await page.screenshot({path: 'error-screen.png'})
+    }
+
+    await browser.close()
   }
 }
 
